aba8952dc592a895b49656cf59b6286a1822f3c0
Leonardo Velazquez <leovelazquez812@gmail.com>
2026-05-07 03:00:40 -0600

    playing with scopes, father singleton, child dep prototype: it doesn't generate a new instance of dep

--- src/main/java/com/lv/spring/basic/scope/JdbcConnection.java
+++ src/main/java/com/lv/spring/basic/scope/JdbcConnection.java
@@ -2,9 +2,12 @@ package com.lv.spring.basic.scope;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class JdbcConnection {
     private static final Logger log = LoggerFactory.getLogger(JdbcConnection.class);
 
